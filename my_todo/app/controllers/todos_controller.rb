ba3297e89ca3ca75c8d1180142c2a7ef182c39c4
BartDeCaluwe <de.caluwe.bart@gmail.com>
2016-01-20 15:14:56 +0100

    Todo saven naar sqlite aangemaakt

--- my_todo/app/controllers/todos_controller.rb
+++ my_todo/app/controllers/todos_controller.rb
@@ -1,2 +1,20 @@
 class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new()
+    @todo.ingavedatum = params[:ingavedatum]
+    @todo.einddatum = params[:einddatum]
+    @todo.prioriteit = params[:prioriteit]
+    @todo.beschrijving = params[:beschrijving]
+    @todo.status = params[:status]
+    @todo.save
+  end
+
+  def show
+
+  end
+
 end
